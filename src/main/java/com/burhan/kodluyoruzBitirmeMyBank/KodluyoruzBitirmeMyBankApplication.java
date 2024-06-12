eb67559e4d74d6648a224a3750aead9b1fa23b70
Burhan Dalgıç <burhandalkic@hotmail.com>
2024-06-12 14:14:25 +0300

    ilk

---
+++ src/main/java/com/burhan/kodluyoruzBitirmeMyBank/KodluyoruzBitirmeMyBankApplication.java
@@ -0,0 +1,13 @@
+package com.burhan.kodluyoruzBitirmeMyBank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KodluyoruzBitirmeMyBankApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KodluyoruzBitirmeMyBankApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/burhan/kodluyoruzBitirmeMyBank/KodluyoruzBitirmeMyBankApplicationTests.java
@@ -0,0 +1,13 @@
+package com.burhan.kodluyoruzBitirmeMyBank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KodluyoruzBitirmeMyBankApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
